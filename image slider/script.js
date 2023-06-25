9b7f07f0b695ae6aa02b30ee449ba9992f42740c
Vicky Kumar <vickyraj0255@gmail.com>
2023-06-25 18:53:47 +0530

    script

---
+++ image slider/script.js
@@ -0,0 +1,13 @@
+let flag = 2;
+
+function controller(x){
+    flag = flag + x;
+
+    flag = 2 + (-1) = 1
+}
+
+slideshow(flag);
+function slideshow(num){
+    let slides = document.getElementsByClassName('slide');
+    slides[num].style.display = "block";
+}
