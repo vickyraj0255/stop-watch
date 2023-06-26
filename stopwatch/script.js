25e78ce460cfdf62472e7d6222243b42e882ca2e
Vicky Kumar <vickyraj0255@gmail.com>
2023-06-27 01:38:05 +0530

    hr min sec

--- stopwatch/script.js
+++ stopwatch/script.js
@@ -14,6 +14,14 @@ function stop(){
 }
 function reset(){
     timer = false;
+    hr = 0;
+    min = 0;
+    sec = 0;
+    count = 0;
+    document.getElementById("hr").innerHTML = "00";
+    document.getElementById("min").innerHTML = "00";
+    document.getElementById("sec").innerHTML = "00";
+    document.getElementById("count").innerHTML = "00";
 }
 
 function stopwatch(){
@@ -34,10 +42,34 @@ function stopwatch(){
             sec = 0;
 
         }
-        document.getElementById("hr").innerHTML = hr;
-        document.getElementById("min").innerHTML = min;
-        document.getElementById("sec").innerHTML = sec;
-        document.getElementById("count").innerHTML = count;
+
+
+        var hrString = hr;
+        var minString = min;
+        var secString = sec;
+        var countString = count;
+
+        if(hr < 10){
+            hrString = "0" + hrString;
+        }
+
+        if(min < 10){
+            minString = "0" + minString;
+        }
+
+        if(sec < 10){
+            secString = "0" + secString;
+        }
+
+        if(count < 10){
+            countString = "0" + countString;
+        }
+
+
+        document.getElementById("hr").innerHTML = hrString;
+        document.getElementById("min").innerHTML = minString;
+        document.getElementById("sec").innerHTML = secString;
+        document.getElementById("count").innerHTML = countString;
 
         setTimeout( "stopwatch()",10);        
     }
